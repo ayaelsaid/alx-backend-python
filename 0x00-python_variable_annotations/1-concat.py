e7620de62dea648e8f4eea37883e21a698312dac
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-04 22:15:31 +0300

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-"""type-annotated function"""
+"""type-annotated function concat"""
 
 
 def concat(str1: str, str2: str) -> str:
-  """takes a float n as argument and returns the floor of the float"""
+  """
+  takes a string str1 and a string str2 as arguments
+  and returns a concatenated string
+  """
     return "{}{}".format(str1, str2)
