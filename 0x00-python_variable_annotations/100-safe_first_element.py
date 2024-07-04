a459e05e535b40eca27fc1081d26ac13b25466e9
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-05 00:14:06 +0300

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
-"""type-annotated function"""
+"""
+module that provides a function for returning the first element
+    of a list, or None if the list is empty
+"""
 from typing import Sequence, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, type(None)]:
-    """correct duck-typed annotations"""
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+"""
+    This function returns the first element of a sequence, or None if the
+        sequence is empty.
+
+    Args:
+    lst (Sequence): The sequence to process.
+
+    Returns:
+    Union[object, None]: The first element of the sequence, or None if the
+        sequence is empty.
+    """
     if lst:
         return lst[0]
     else:
