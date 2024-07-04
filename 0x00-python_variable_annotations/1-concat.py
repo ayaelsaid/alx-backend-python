e8ece007882f65ee70721e5e623b1461e7211b27
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-04 22:17:09 +0300

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,8 +3,5 @@
 
 
 def concat(str1: str, str2: str) -> str:
-  """
-  takes a string str1 and a string str2 as arguments
-  and returns a concatenated string
-  """
-    return "{}{}".format(str1, str2)
+  """takes a two string and returns a concatenated string"""
+    return str1 + str2
