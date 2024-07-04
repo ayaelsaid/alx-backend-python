ac67c642c80f4ddfcbf55bb9355df94d1503432b
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-04 22:08:26 +0300

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,11 +5,9 @@
 def add(a: float, b: float) -> float:
     """
     Returns the sum of two floats.
-    
     Args:
     a (float): The first float number.
     b (float): The second float number.
-    
     Returns:
     float: The sum of a and b.
     """
