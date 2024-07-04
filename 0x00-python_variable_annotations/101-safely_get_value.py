3f5176c153c89154911fc1c076d53e2ed6f976f0
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-05 00:09:30 +0300

    Update 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -11,19 +11,17 @@ Default = Union[T, None]
 
 def safely_get_value(dct: Mapping, key: Any, default: Default = None) -> Result:
     """
-    This function gets the value for a given key in a mapping, or returns
-        a default value if the key is not in the mapping.
+    Safely retrieves a value from a dictionary.
 
     Parameters:
-    dct (Mapping): The mapping to get the value from.
-    key (Any): The key to look up in the mapping.
-    default (Union[T, None]): The default value to return if the key is not
-        in the mapping.
+    - dct (Mapping): The dictionary to retrieve values from.
+    - key (Any): The key to look up in the dictionary.
+    - default (Union[T, None]): The default value to return if the key is not found (default is None).
 
     Returns:
-    Union[Any, T]: The value for the key in the mapping, or the default
-        value if the key is not in the mapping.
+    - Union[T, None]: The value corresponding to the key in the dictionary, or the default value if key is not found.
     """
+
     if key in dct:
         return dct[key]
     else:
