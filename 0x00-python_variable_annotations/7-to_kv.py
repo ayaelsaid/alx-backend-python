d67cee8781cb4cf2b010ec4b3d03a6120d80babc
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-04 22:46:12 +0300

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,4 +5,4 @@ from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ takes a string k and an int OR float v as arguments and returns a tuple""
-    return (k, v**2)
+    return (k, v ** 2)
